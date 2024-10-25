c9d79e50772f7bf1c4e9eefd6f1e85b311b30903
MarekLempart <kama.zakupy@gmail.com>
2024-10-25 10:45:38 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/temp_Vite-TypeScript/",
+  base: "/photoStudio/",
   plugins: [react()],
 });
