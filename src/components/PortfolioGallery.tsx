fa3bc040c601751653f1c3525f7312280f9604a5
MarekLempart <kama.zakupy@gmail.com>
2025-01-21 15:28:29 +0100

    fix

--- src/components/PortfolioGallery.tsx
+++ src/components/PortfolioGallery.tsx
@@ -7,19 +7,19 @@ import imagePortfolioData from '../data/imagePortfolioData';
 import { GalleryContainer, Thumbnail, ThumbnailWrapper } from '../styles/PortfolioGalleryStyles';
 
 const PortfolioGallery: React.FC = () => {
-  return (
-    <PhotoProvider>
-      <GalleryContainer>
-        {imagePortfolioData.map((image) => (
-          <ThumbnailWrapper key={image.id}>
-            <PhotoView src={image.src}>
-              <Thumbnail src={image.src} alt={image.alt} />
-            </PhotoView>
-          </ThumbnailWrapper>
-        ))}
-      </GalleryContainer>
-    </PhotoProvider>
-  );
+    return (
+        <PhotoProvider>
+        <GalleryContainer>
+            {imagePortfolioData.map((image) => (
+            <ThumbnailWrapper key={image.id}>
+                <PhotoView src={image.src}>
+                <Thumbnail src={image.src} alt={image.alt} />
+                </PhotoView>
+            </ThumbnailWrapper>
+            ))}
+        </GalleryContainer>
+        </PhotoProvider>
+    );
 };
 
 export default PortfolioGallery;
