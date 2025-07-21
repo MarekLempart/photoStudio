7945da72b1a24e1eabbf412331390edf63461d2b
MarekLempart <kama.zakupy@gmail.com>
2025-07-21 13:21:23 +0200

    slider fix

--- src/styles/ImageSliderStyles.ts
+++ src/styles/ImageSliderStyles.ts
@@ -27,14 +27,19 @@ export const SliderWrapper = styled.div`
         border-radius: 8px;
     }
 
+    .slick-list {
+        margin: 0;
+        overflow: hidden;
+    }
+
     .slick-track {
         display: flex;
     }
 
-    & .slick-list {
+    /* & .slick-list {
         margin: 0;
         overflow: hidden;
-    }
+    } */
 
     @media (max-width: 768px) {
         width: 100%;
