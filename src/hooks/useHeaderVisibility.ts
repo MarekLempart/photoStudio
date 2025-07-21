31474626705f61358e52cd2aec78d6efea12a1a0
MarekLempart <kama.zakupy@gmail.com>
2025-07-21 09:51:30 +0200

    header fix

--- src/hooks/useHeaderVisibility.ts
+++ src/hooks/useHeaderVisibility.ts
@@ -9,33 +9,33 @@ export const useHeaderVisibility = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      // const currentScrollY = window.scrollY;
-
-      // if (currentScrollY <= SCROLL_DELTA) {
-      //   setIsVisible(true);
-      //   lastScrollY.current = currentScrollY;
-      //   return;
-      // }
-
-      // if (Math.abs(currentScrollY - lastScrollY.current) < SCROLL_DELTA) {
-      //   return;
-      // }
-      // const isScrollingDown = currentScrollY > lastScrollY.current;
-      // setIsVisible(!isScrollingDown);
-      // lastScrollY.current = currentScrollY;
       const currentScrollY = window.scrollY;
+
+      if (currentScrollY <= SCROLL_DELTA) {
+        setIsVisible(true);
+        lastScrollY.current = currentScrollY;
+        return;
+      }
 
       if (Math.abs(currentScrollY - lastScrollY.current) < SCROLL_DELTA) {
         return;
       }
-
-      if (currentScrollY < 10 || currentScrollY < lastScrollY.current) {
-        setIsVisible(true);
-      } else {
-        setIsVisible(false);
-      }
-
+      const isScrollingDown = currentScrollY > lastScrollY.current;
+      setIsVisible(!isScrollingDown);
       lastScrollY.current = currentScrollY;
+      // const currentScrollY = window.scrollY;
+
+      // if (Math.abs(currentScrollY - lastScrollY.current) < SCROLL_DELTA) {
+      //   return;
+      // }
+
+      // if (currentScrollY < 10 || currentScrollY < lastScrollY.current) {
+      //   setIsVisible(true);
+      // } else {
+      //   setIsVisible(false);
+      // }
+
+      // lastScrollY.current = currentScrollY;
     };
 
     window.addEventListener("scroll", handleScroll, { passive: true });
