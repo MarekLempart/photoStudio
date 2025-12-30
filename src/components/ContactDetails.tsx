930682a8099b81c9561e1e1c84585fd829e1107c
MarekLempart <kama.zakupy@gmail.com>
2025-12-30 10:40:18 +0100

    fix

--- src/components/ContactDetails.tsx
+++ src/components/ContactDetails.tsx
@@ -48,7 +48,7 @@ const ContactDetails: React.FC = () => {
           <FaEnvelope />
         </DetailIcon>
         <DetailText>
-          <a href="mailto:marta_mm@wp.pl">kontakt@mmphoto.com</a>
+          <a href="mailto:marta_mm@wp.pl">kontakt@mmphoto.org</a>
         </DetailText>
       </DetailItem>
 
